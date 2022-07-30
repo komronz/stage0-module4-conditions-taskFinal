2531ee835601d1394d053a1b6d2127e87cb2d005
komronz <pandacoder0820@gmail.com>
2022-07-30 16:40:48 +0500

    Module4FinalTask

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(year >= 0  && month > 0 && month <13) {
+            boolean leap = false;
+            if (year % 4 == 0) {
 
+                if (year % 100 == 0) {
+                    if (year % 400 == 0)
+                        leap = true;
+                } else
+                    leap = true;
+            }
+            int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+            if((leap) && month == 2)
+                System.out.println(29);
+            else
+                System.out.println(days[month-1]);
+
+        }else {
+            System.out.println("invalid date");
+        }
     }
 }
